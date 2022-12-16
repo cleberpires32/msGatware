cfed32e168ecd64d43cd2b73521c378949315ba7
cleberpires32 <cleberpires269@gmail.com>
2022-12-16 10:03:11 -0300

    Criação de Gatware para micro serviços cartão / cliente

---
+++ src/test/java/com/curso/eureka/msGatware/MsGatwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.eureka.msGatware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
